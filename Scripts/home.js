a074e2df8ae4a0ffd7f12e3356f721d73580b67b
Rob White <rob.j.white@gmail.com>
2014-02-23 09:08:11 +0000

    Added missing content file

---
+++ Scripts/home.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () { //Start up our Featured Project Carosuel
+    var interval;
+    $('#featured ul')
+        .roundabout({
+            reflect: false,
+            easing: 'easeInOutCirc',
+            duration: 1300,
+            minOpacity: 0.0,
+            maxOpacity: 1.0
+        })
+        .hover(
+            function () {
+                // pause
+				clearInterval(interval);
+			},
+			function () {
+                // go
+			    interval = startAutoPlay();
+			});
+
+    // start auto rotate
+    interval = startAutoPlay();
+});
+
+function startAutoPlay() {
+    return setInterval(function () {
+        $('#featured ul').roundabout_animateToNextChild();
+    }, 5000);
+}
